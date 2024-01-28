ee8fc3987af96863982f4c1b04e7b6f28ceb3851
SerenityZenDev <tjrqo2000@gmail.com>
2024-01-28 17:26:52 +0900

    :tada: init project

---
+++ src/main/java/com/sparta/secureschedulerappserver/SecureSchedulerAppServerApplication.java
@@ -0,0 +1,13 @@
+package com.sparta.secureschedulerappserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecureSchedulerAppServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SecureSchedulerAppServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sparta/secureschedulerappserver/SecureSchedulerAppServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sparta.secureschedulerappserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecureSchedulerAppServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
