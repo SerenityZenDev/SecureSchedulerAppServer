409bcb08de93d2f4282622b5cc722626d66be20d
SerenityZenDev <tjrqo2000@gmail.com>
2024-01-30 15:37:32 +0900

    :sparkles: 회원가입 HTTP 코드 추가 및 회원가입 기능 필터로 이동

--- src/main/java/com/sparta/secureschedulerappserver/controller/UserController.java
+++ src/main/java/com/sparta/secureschedulerappserver/controller/UserController.java
@@ -6,9 +6,13 @@ import com.sparta.secureschedulerappserver.entity.User;
 import com.sparta.secureschedulerappserver.service.UserService;
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.validation.Valid;
+import java.util.ArrayList;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -20,28 +24,34 @@ import org.springframework.web.bind.annotation.RestController;
 public class UserController {
     private final UserService userService;
     @PostMapping("/join")
-    public ResponseEntity<UserResponseDto> join (@Valid @RequestBody UserRequestDto userRequestDto){
-        try {
-            userService.join(userRequestDto);
-            String successMessage = "회원가입이 성공적으로 완료되었습니다.";
-            return new ResponseEntity<>(new UserResponseDto(successMessage, HttpStatus.OK), HttpStatus.OK);
-        } catch (Exception e) {
-            String errorMessage = "회원가입 중 오류가 발생했습니다. 다시 시도해주세요.";
-            return new ResponseEntity<>(new UserResponseDto(errorMessage, HttpStatus.INTERNAL_SERVER_ERROR), HttpStatus.INTERNAL_SERVER_ERROR);
+    public ResponseEntity<List<UserResponseDto>> join (@Valid @RequestBody UserRequestDto userRequestDto, BindingResult bindingResult){
+        List<FieldError> fieldErrors = bindingResult.getFieldErrors();
+        List<UserResponseDto> userResponseDtos = new ArrayList<>();
+        if (fieldErrors.size() > 0){
+            for (FieldError fieldError : fieldErrors){
+                userResponseDtos.add(new UserResponseDto(fieldError.getField() +"을 "+fieldError.getDefaultMessage(), HttpStatus.UNAUTHORIZED));
+            }
+            return new ResponseEntity<>(userResponseDtos, HttpStatus.UNAUTHORIZED);
         }
+        userService.join(userRequestDto);
+        String successMessage = "회원가입이 성공적으로 완료되었습니다.";
+        userResponseDtos.add(new UserResponseDto(successMessage, HttpStatus.OK));
+
+        return new ResponseEntity<>(userResponseDtos, HttpStatus.OK);
     }
 
-    @PostMapping("/login")
-    public ResponseEntity<UserResponseDto> login(
-        @Valid @RequestBody UserRequestDto userRequestDto,
-        HttpServletResponse res){
-        try {
-            userService.login(userRequestDto, res);
-            String successMessage = "로그인이 성공적으로 완료되었습니다.";
-            return new ResponseEntity<>(new UserResponseDto(successMessage, HttpStatus.OK), HttpStatus.OK);
-        } catch (Exception e) {
-            String errorMessage = "로그인 중 오류가 발생했습니다. 다시 시도해주세요.";
-            return new ResponseEntity<>(new UserResponseDto(errorMessage, HttpStatus.INTERNAL_SERVER_ERROR), HttpStatus.INTERNAL_SERVER_ERROR);
-        }
-    }
+// 필터로 이동
+//    @PostMapping("/login")
+//    public ResponseEntity<UserResponseDto> login(
+//        @Valid @RequestBody UserRequestDto userRequestDto,
+//        HttpServletResponse res){
+//        try {
+//            userService.login(userRequestDto, res);
+//            String successMessage = "로그인이 성공적으로 완료되었습니다.";
+//            return new ResponseEntity<>(new UserResponseDto(successMessage, HttpStatus.OK), HttpStatus.OK);
+//        } catch (Exception e) {
+//            String errorMessage = "로그인 중 오류가 발생했습니다. 다시 시도해주세요.";
+//            return new ResponseEntity<>(new UserResponseDto(errorMessage, HttpStatus.INTERNAL_SERVER_ERROR), HttpStatus.INTERNAL_SERVER_ERROR);
+//        }
+//    }
 }
--- src/main/java/com/sparta/secureschedulerappserver/service/UserService.java
+++ src/main/java/com/sparta/secureschedulerappserver/service/UserService.java
@@ -31,22 +31,22 @@ public class UserService {
         userRepository.save(user);
     }
 
-    public void login(UserRequestDto userRequestDto, HttpServletResponse res) {
-        String username = userRequestDto.getUsername();
-        String password = userRequestDto.getPassword();
-
-        // 사용자 확인
-        User user = userRepository.findByUsername(username).orElseThrow(
-            () -> new IllegalArgumentException("등록된 사용자가 없습니다.")
-        );
-
-        // 비밀번호 확인
-        if (!passwordEncoder.matches(password, user.getPassword())){
-            throw new IllegalArgumentException("비밀번호가 일치하지 않습니다.");
-        }
-
-        // JWT 생성 및 쿠키에 저장 후 Response 객체에 추가
-        String token = jwtUtil.createToken(user.getUsername());
-        jwtUtil.addJwtToCookie(token, res);
-    }
+//    public void login(UserRequestDto userRequestDto, HttpServletResponse res) {
+//        String username = userRequestDto.getUsername();
+//        String password = userRequestDto.getPassword();
+//
+//        // 사용자 확인
+//        User user = userRepository.findByUsername(username).orElseThrow(
+//            () -> new IllegalArgumentException("등록된 사용자가 없습니다.")
+//        );
+//
+//        // 비밀번호 확인
+//        if (!passwordEncoder.matches(password, user.getPassword())){
+//            throw new IllegalArgumentException("비밀번호가 일치하지 않습니다.");
+//        }
+//
+//        // JWT 생성 및 쿠키에 저장 후 Response 객체에 추가
+//        String token = jwtUtil.createToken(user.getUsername());
+//        jwtUtil.addJwtToCookie(token, res);
+//    }
 }
